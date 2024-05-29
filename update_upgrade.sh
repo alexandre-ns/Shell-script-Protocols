e06a5afe73a497c33a9fff61c45d9f03c3ff0d9d
alexandre <alexandren49@gmail.com>
2024-05-29 00:22:41 -0300

    enable service

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -6,8 +6,12 @@ touch ~/update.log
 #creates a unit of service using the update.service file
 sudo cp static/update.service /etc/systemd/system/
 
+#copy script file
+sudo cp script_update.sh ~/.script_update.sh
+chmod +x ~/.script_update.sh
 
-sudo cp script_update.sh   ~/.script_update.sh
+#reload the systemd daemon
+sudo systemctl daemon-reload
 
-
-#chmod +x /usr/local/bin/meu_script.sh
+#enable unit of service
+sudo systemctl enable update.service
